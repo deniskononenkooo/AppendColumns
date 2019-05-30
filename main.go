18fc8d03df8cff498d7301f1ee627a999c392c06
deniskononenkoooo <denys.kononenko@continuum.net>
2019-05-30 14:40:02 +0300

    Created script for appending columns

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+//export
+//copy retries ("ID","RetryID","Action","Payload","RetryCount","Type") to 'dump.csv' with delimiter = '|';
+//
+//import
+//copy retries ("ID","RetryID","Action","Payload","RetryCount","Type","Timestamp","BucketID","ErrorMessage","UpdatedAt") from 'results.csv' with delimiter='|' and maxbatchsize = 1 ;
+
+func main() {
+	readerFile, err := os.Open("./dump.csv")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer readerFile.Close()
+
+	reader := csv.NewReader(readerFile)
+	reader.Comma = '|'
+	reader.LazyQuotes = true
+
+	writerFile, err := os.OpenFile("./result.csv", os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer writerFile.Close()
+
+	writer := csv.NewWriter(writerFile)
+	writer.Comma = '|'
+
+	text, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	timestamp := time.Now()
+
+	for _, line := range text {
+		t := timestamp.Format("2006-01-02 15:04:05")
+
+		bucketID := int(time.Duration(timestamp.UnixNano()).Minutes() / 15)
+
+		line = append(line, t, strconv.Itoa(bucketID), "", t) // append Timestamp, BucketID, ErrorMessage, UpdatedAt
+
+		err = writer.Write(line)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		timestamp = timestamp.Add(-1 * time.Second)
+	}
+
+	writer.Flush()
+}
